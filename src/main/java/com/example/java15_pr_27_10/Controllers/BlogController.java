c6c4a9bfe8907159b1f5167c636c47996225f6a2
The Waggle <artuoh76@gmail.com>
2023-10-30 21:07:29 +0100

    post create

--- src/main/java/com/example/java15_pr_27_10/Controllers/BlogController.java
+++ src/main/java/com/example/java15_pr_27_10/Controllers/BlogController.java
@@ -1,21 +1,82 @@
 package com.example.java15_pr_27_10.Controllers;
 
+import com.azure.storage.blob.*;
+import com.azure.storage.blob.BlobServiceClientBuilder;
+import com.azure.storage.blob.models.BlobAccessPolicy;
+import com.azure.storage.blob.models.BlobSignedIdentifier;
+import com.azure.storage.blob.models.PublicAccessType;
 import com.example.java15_pr_27_10.Models.Post;
 import com.example.java15_pr_27_10.repo.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 @Controller
 public class BlogController {
     @Autowired
     private PostRepository postRepository;
+    private BlobServiceClient client;
+    private BlobContainerClient container;
+    public BlogController(){
+        BlobServiceClientBuilder builder = new BlobServiceClientBuilder();
+        client = builder.connectionString("UseDevelopmentStorage=true").buildClient();
+        BlobSignedIdentifier identifier = new BlobSignedIdentifier().setId("test policy")
+                .setAccessPolicy(new BlobAccessPolicy().setStartsOn(OffsetDateTime.now())
+                        .setExpiresOn(OffsetDateTime.now().plusDays(7))
+                        .setPermissions("cd")); //permission for create and delete
+
+        ArrayList<BlobSignedIdentifier> identifiers = new ArrayList<BlobSignedIdentifier>();
+        identifiers.add(identifier);
+        container = client.createBlobContainerIfNotExists("posts");
+        container.setAccessPolicy(PublicAccessType.CONTAINER,identifiers);
+    }
     @GetMapping("/posts")
     public String Post(Model model){
         Iterable<Post> posts = postRepository.findAll();
         model.addAttribute("list",posts);
         return "posts";
+    }
+    @GetMapping("/posts/create")
+    public String create(Model model)
+    {
+        return "create";
+    }
+    @PostMapping("/posts/create")
+    public String create(@RequestParam String header, @RequestParam String context, @RequestParam MultipartFile image, Model model ) throws FileNotFoundException {
+        Post post = new Post();
+        post.setContext(context);
+        post.setHeader(header);
+//        String path = new ClassPathResource("resources/static/").getPath() +image.getOriginalFilename();
+        try(
+                var stream = image.getInputStream();
+                )
+        {
+            BlobClient cl = container.getBlobClient(image.getOriginalFilename());
+            cl.upload(stream,stream.available(),true);
+            post.setImage(cl.getBlobUrl());
+        }
+        catch (Exception ex)
+        {
+            System.out.println(ex.getMessage());
+        }
+
+
+        postRepository.save(post);
+        return "redirect:/posts";
     }
 
 }
--- src/main/java/com/example/java15_pr_27_10/Models/Post.java
+++ src/main/java/com/example/java15_pr_27_10/Models/Post.java
@@ -2,26 +2,33 @@ package com.example.java15_pr_27_10.Models;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.boot.context.properties.bind.DefaultValue;
+import lombok.Setter;
 
 import java.sql.Timestamp;
-
-import static java.lang.System.currentTimeMillis;
+import  java.util.Date;
 
 @Entity
 @Getter
 @AllArgsConstructor @NoArgsConstructor
 @Table(name = "Posts")
+@Setter
 public class Post  {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String header;
+    @Column(columnDefinition = "TEXT")
     private String context;
-    private Timestamp date = new Timestamp(currentTimeMillis());
+    private String image;
+
+    @Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP", insertable = false, updatable = false)
+    private Timestamp date = new Timestamp(System.currentTimeMillis());
+    @PrePersist
+    protected void onCreate() {
+        date = new Timestamp(new Date().getTime());
+    }
 
 
 }
